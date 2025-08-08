5cc781ea1b984dfb32b943a27269e9d814f1ce98
Dikin19 <muhamadsodikin191919@gmail.com>
2025-08-08 17:02:54 +0700

    anggotaBom

---
+++ domAndBom.js
@@ -0,0 +1,37 @@
+// METHOD ALERT
+// alert akan jalan ditampilan website karena sudah connect dengan html dengan memberi script js di file html
+// const tamu = prompt('siapakah anda')
+// alert(`selamat datang,  ${tamu} dirumah !`)
+
+
+// Silakan aktifkan kode di bawah ini dengan hapus komentar untuk melihat hasilnya
+// alert('Halo, user!');
+
+// let message = 'Halo, bom method dari alert yang disimpan dalam variabel!';
+// Silakan aktifkan kode di bawah ini dengan hapus komentar untuk melihat hasilnya
+// alert(message);
+
+
+// METHOD PROMPT
+// silahkan aktifkan kode dibawah ini dengan hapus komentar
+// let inputMessage = prompt('input your message')
+// console.log(inputMessage); // menampilkan hasil prompt diterminal
+// alert(`berhasil ${inputMessage}`) // menampilkan ke tampilan browser
+
+
+// Silakan aktifkan kode di bawah ini dengan hapus komentar untuk melihat hasilnya
+// let pesanInput1 = prompt('Silakan masukkan angka...');
+// console.log(`Tipe data dari \`pesanInput\` adalah ${typeof pesanInput1}`);
+// console.log(pesanInput1);
+// HASIL adalah string
+
+// Silakan aktifkan kode di bawah ini dengan hapus komentar untuk melihat hasilnya
+// let pesanInput = prompt('Silakan masukkan angka yang Anda suka!');
+// let pesanInputConvertedToNumber = Number(pesanInput);
+// console.log(`Tipe data dari \`pesanInputConvertedToNumber\` adalah ${typeof pesanInputConvertedToNumber}`);
+// console.log(pesanInputConvertedToNumber);
+// Hasil adalah number
+
+// Silakan hapus dua komentar di bawah ini untuk melihat hasilnya
+// let name = prompt('Silakan masukkan nama Anda!', 'John Doe');
+// console.log(name);
